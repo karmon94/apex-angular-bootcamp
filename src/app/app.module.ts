ff0a96b21c5ec3ed2b4d9995172ab45abe2811b9
Rogelio Carrillo <rcarrillo@quantumx.mx>
2024-03-20 17:18:51 -0600

    Directivas de atributo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DirectivasDeAtributoComponent } from './components/directivas-de-atributo/directivas-de-atributo.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, DirectivasDeAtributoComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
