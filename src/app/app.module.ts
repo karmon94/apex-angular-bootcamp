4e1e1b9b50f9f639af916f229c4d6756fed9a677
Rogelio Carrillo <rcarrillo@quantumx.mx>
2024-03-20 20:41:47 -0600

    Clean up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DirectivasDeAtributoComponent } from './components/directivas-de-atributo/directivas-de-atributo.component';
-import { DirectivasEstructuralesComponent } from './components/directivas-estructurales/directivas-estructurales.component';
-import { DirectivasCustomComponent } from './components/directivas-custom/directivas-custom.component';
-import { DataBindingComponent } from './components/data-binding/data-binding.component';
 import { MyFirstModule } from './modules/my-first-module.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    DirectivasDeAtributoComponent,
-    DirectivasEstructuralesComponent,
-    DirectivasCustomComponent,
-    DataBindingComponent,
-    MyFirstModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, MyFirstModule],
   providers: [],
   bootstrap: [AppComponent],
 })
