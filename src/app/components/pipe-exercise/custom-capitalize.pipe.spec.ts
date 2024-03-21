3a3a18f292b9f6c459b491c35df5345238384575
Rogelio Carrillo <rcarrillo@quantumx.mx>
2024-03-20 20:48:26 -0600

    Clean up

--- src/app/components/pipe-exercise/custom-capitalize.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CustomCapitalizePipe } from './custom-capitalize.pipe';
-
-describe('CustomCapitalizePipe', () => {
-  it('create an instance', () => {
-    const pipe = new CustomCapitalizePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
