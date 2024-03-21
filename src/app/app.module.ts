b62d2472365612a12901c1ac13568312be09cefd
Rogelio Carrillo <rcarrillo@quantumx.mx>
2024-03-20 18:43:42 -0600

    custom pipe and built in implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DirectivasDeAtributoComponent } from './components/directivas-de-atributo/directivas-de-atributo.component';
-import { DirectivasEstructuralesComponent } from './components/directivas-estructurales/directivas-estructurales.component';
-import { DirectivasCustomComponent } from './components/directivas-custom/directivas-custom.component';
-import { DataBindingComponent } from './components/data-binding/data-binding.component';
-import { MyFirstModule } from './modules/my-first-module.module';
+import { PipeExerciseComponent } from './components/pipe-exercise/pipe-exercise.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    DirectivasDeAtributoComponent,
-    DirectivasEstructuralesComponent,
-    DirectivasCustomComponent,
-    DataBindingComponent,
-    MyFirstModule,
-  ],
+  imports: [BrowserModule, PipeExerciseComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
